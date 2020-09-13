d1c2d6f19d2f9486ebaadec737517c3882cafc57
RISHABH MISHRA <dmrishabh@users.noreply.github.com>
2020-09-13 22:12:54 +0530

    version 2

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+
+// You need to include some header(s) here!
+
+# include <iostream>
+
+// You need to define your main() function here!
+int main(){
+    
+    std::cout<<"Hello, world! \n Greetings from Illinois !"<<std::endl;
+    
+    return 0;
+}
+
